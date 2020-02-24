78cf4969881fbc4bcbde47dad6390d3d9cc41a43
¨ezgioz¨ <ezgiozsogut@gmail.com>
2020-02-24 16:53:07 +0100

    Put define inital calib from previous

--- BGM_solver.py
+++ BGM_solver.py
@@ -73,47 +73,78 @@ def compute_case(expm):
         
         # Solve for the competitive equilibrium
         model.set_calibration(po=0);
-        calib = find_steady_state(model);
-        res = residuals(model,calib=calib)
+        calib_ce = find_steady_state(model);
+        res = residuals(model,calib=calib_ce)
         
         
         if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
-            print('model', case, 'ME', param, '=', i, '--> steady state cant be found: non-zero residuals')  
-  
-            
-        dr_ce = perturb(model, order=2, steady_state=calib);
+            #print('model', case, 'ME', freeparam, '=', i, '--> steady state cant be found: non-zero residuals')  
+            calib_ce = calib_pre_ce
+            res = residuals(model,calib=calib_ce)
+            if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
+                print('model', case, 'ME', freeparam, '=', i, '--> cant be solved even with previous value calib')  
+                dr_me_true = 0 
+            else: 
+                dr_me_true = 1         
+                dr_ce = perturb(model, order=2, steady_state=calib_ce);
+                
+        else:    
+            #print('model', case, 'ME', freeparam, '=', i, '--> steady state found')  
+            dr_me_true = 1         
+            dr_ce = perturb(model, order=2, steady_state=calib_ce);
 
         # Solve for the social planner equilibrium
         model.set_calibration(po=1);
-        calib = find_steady_state(model);
-        res = residuals(model,calib=calib)
+        calib_po = find_steady_state(model);
+        res = residuals(model,calib=calib_po)
+        
         if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
-            print('model', case, 'PO', param, '=', i, '--> steady state cant be found: non-zero residuals')
-      
-        
-        dr_po = perturb(model, order=2, steady_state=calib);   
-        
-                    
-            
-        # Initial point at which to evaluate the welfare
-        eval_point = calib['states'].copy();
-        # Change initial value of N
-        eval_point[1]*=0.5 # to 50 % of its of its steady-state value
-        #eval_point[1]= 1    # to 1
+            #print('model', case, 'PO', freeparam, '=', i, '--> steady state cant be found: non-zero residuals')
+            calib_po = calib_pre_po
+            res = residuals(model,calib=calib_po)
+            if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
+                print('model', case, 'PO', freeparam, '=', i, '--> cant be solved even with previous value calib')  
+                dr_po_true = 0 
+            else: 
+                dr_po_true = 1         
+                dr_po = perturb(model, order=2, steady_state=calib_po);
 
-        # Evalueate welfare
-        w_ce = dr_ce(eval_point)[1]
-        w_po = dr_po(eval_point)[1]
+        else:
+            #print('model', case, 'PO', freeparam, '=', i, '--> steady state found')  
+            dr_po_true = 1
+            dr_po = perturb(model, order=2, steady_state=calib_po);   
+            
+        print('model_ME_ss', calib_ce, 'model_PO_ss', calib_po)   
+                    
+        if dr_me_true == 1 and dr_po_true == 1:
+            if i < 0.35:
+                print('model', freeparam, '=', i, '--> steady state found for both models')
+            # Initial point at which to evaluate the welfare
+            eval_point = calib['states'].copy();
+            # Change initial value of N
+            eval_point[1]*=0.5 # to 50 % of its of its steady-state value
+            #eval_point[1]= 1    # to 1
+            
+            # Evalueate welfare
+            w_ce = dr_ce(eval_point)[1]
+            w_po = dr_po(eval_point)[1]
+            
+            # Convert to consumption equivalent
+            v_ce = exp((1-beta)*w_ce)
+            v_po = exp((1-beta)*w_po)
+            
+        else:
+            v_ce = float('NaN')
+            v_po = float('NaN')
         
-        # Convert to consumption equivalent
-        v_ce = exp((1-beta)*w_ce)
-        v_po = exp((1-beta)*w_po)
+        
+        calib_pre_ce = calib_ce
+        calib_pre_po = calib_po
         
         # Add to list of values
         values_ce.append(v_ce)
         values_po.append(v_po)
-    
-   
+        
     #Construct dataframe
     columns = [h.format(case) for h in ['{}', '{}_po', '{}_ce']]
     df = pandas.DataFrame( column_stack([parm_vec, values_po, values_ce]), columns=columns)
