db972f1e9718435219900204a4f89a16d7417270
¨ezgioz¨ <ezgiozsogut@gmail.com>
2020-02-26 01:39:32 +0100

    All files cleaned up

--- BGM_solver.py
+++ BGM_solver.py
@@ -34,20 +34,39 @@ def compute_case(expm):
     import sympy
     from sympy import  Symbol, solve
     
+    ################################
+    # Set information for each model
+    ################################
+    
+    ### Code below will set information for each case and name output variables/files accordingly
+    # If model is CES to be solved for free parameter xi, declarre in Produce graphs notebook:
+    # dict(modfile='ces', freeparam='xi', range=(0.2,1.0,N))
+    # output files will be names as 'BGM_' 
+    #
+    
+    ### Model file name, Freeparam and range --> we will solve model for each value of the declared interval
     freeparam = expm['freeparam']
     range = expm['range']
     modfile = expm['modfile']
+    
+    ### Fixed param--> if any, default is from model calibration in the yaml file
+    # (Here we solve translog model twice with two different varphi values so fixed parameter exist)
     if 'fixedparam' in expm:
         fixedparam = expm['fixedparam']
         fixedparamval = expm['value']
         case = f"{expm['modfile']}_{expm['fixedparam']}{expm['value']}_{expm['freeparam']}".replace('.', '')
     else:
         case = f"{expm['modfile']}_{expm['freeparam']}".replace('.', '')
-
+    
+    
+    #######################
+    # Solving for each model
+    #########################
+    
     # Import model    
     model = yaml_import('model_files/model_{}.yaml'.format(modfile))
     
-    # Set fixed parameter declared above (Here we solve translog model twice with two different varphi values)
+    # Set fixed parameter declared above 
     if 'fixedparam' in expm:
         p2 = {expm['fixedparam']:expm['value']}
         model.set_calibration(**p2)
@@ -55,12 +74,10 @@ def compute_case(expm):
     # Set beta    
     beta = model.calibration['beta'];
     
-    # Create necesarry objects to store results
+    # Create necesarry objects to store results -> This will store consumption equivalent
+    # of welfare for competetitive eq. and planner eq.
     values_ce = []
     values_po = []
-
-    nvals_ce = []
-    nvals_po = []
     
     # create parameter range
     parm_vec = linspace(*range)
@@ -72,65 +89,52 @@ def compute_case(expm):
         pp = {freeparam:i}
         model.set_calibration(**pp)
         
-        
+        #######################################
         # Solve for the competitive equilibrium
+        ######################################### 
         model.set_calibration(po=0);
         calib_ce = find_steady_state(model);
         res = residuals(model,calib=calib_ce)
         
         
         if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
-            #print('model', case, 'ME', freeparam, '=', i, '--> steady state cant be found: non-zero residuals')  
+            print('model', case, 'ME', freeparam, '=', i, '--> steady state cant be found: non-zero residuals')  
             dr_me_true = 0
-            
-            #calib_ce = calib_pre_ce
-            #res = residuals(model,calib=calib_ce)
-            #if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
-            #    dr_me_true = 0   
-            #else: 
-            #    dr_po_true = 1
-            #    dr_ce = perturb(model, order=2, steady_state=calib_ce);
-            #    N_ce = model.calibration['N']
 
         else:    
-            #print('model', case, 'ME', freeparam, '=', i, '--> steady state found')  
             dr_me_true = 1         
             dr_ce = perturb(model, order=2, steady_state=calib_ce);
-            N_ce = model.calibration['N']
 
+        ##########################################    
         # Solve for the social planner equilibrium
+        ########################################## 
         model.set_calibration(po=1);
         calib_po = find_steady_state(model);
         res = residuals(model,calib=calib_po)
         
         if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
             dr_po_true = 0
-            
             #print('model', case, 'ME', freeparam, '=', i, '--> steady state cant be found: non-zero residuals')  
-            #calib_po = calib_pre_po
-            #res = residuals(model,calib=calib_po)
-            #if abs(sum(res['arbitrage'])) + abs(sum(res['transition'])) > 0.00001:
-            #    dr_po_true = 0   
-            #else:  
-            #    dr_po_true = 1
-            #    dr_po = perturb(model, order=2, steady_state=calib_po);
-            #    N_po = model.calibration['N']
+            
 
         else:
-            #print('model', case, 'PO', freeparam, '=', i, '--> steady state found')  
             dr_po_true = 1
             dr_po = perturb(model, order=2, steady_state=calib_po);   
-            N_po = model.calibration['N']
-                    
+        
+        # If model is solved correctly for both cases
+        # compute welfare and gains for that parameter value
+        
         if dr_me_true == 1 and dr_po_true == 1:
-            print('model', freeparam, '=', i, '--> steady state found for both models')
-            # Initial point at which to evaluate the welfare
+            #print('model', freeparam, '=', i, '--> steady state found for both models')
+            
+            # Initial point at which to evaluate the welfare (common for both models!)
             eval_point = calib_po['states'].copy();
+            
             # Change initial value of N
             eval_point[1]*=0.5 # to 50 % of its of its steady-state value
             #eval_point[1]= 1    # to 1
             
-            # Evalueate welfare
+            # Evalueate welfare at this point (same point) for both ce and po
             w_ce = dr_ce(eval_point)[1]
             w_po = dr_po(eval_point)[1]
                 
@@ -139,25 +143,20 @@ def compute_case(expm):
             v_po = exp((1-beta)*w_po)
             
             
-        else:
+        else: #If model is not solved for both cases, set it to NaN
             v_ce = float('NaN')
             v_po = float('NaN')
             
-            N_ce = float('NaN')
-            N_po = float('NaN')
             
         # Add to list of values
         values_ce.append(v_ce)
         values_po.append(v_po)
         
-        nvals_ce.append(N_ce)
-        nvals_po.append(N_po)
+    #Construct dataframe -> Create the table with param value, cons. eq. of welfare for each 
+    columns = [h.format(case) for h in ['{}', '{}_po', '{}_ce']]
+    df = pandas.DataFrame( column_stack([parm_vec, values_po, values_ce]), columns=columns)
     
-    #Construct dataframe
-    columns = [h.format(case) for h in ['{}', '{}_po', '{}_ce', '{}_N_po', '{}_N_ce']]
-    df = pandas.DataFrame( column_stack([parm_vec, values_po, values_ce, nvals_po, nvals_ce]), columns=columns)
-    
-    #compute gains
+    #compute gains -> compute gains and append matrix
     df['{}_gain'.format(case)] = (df['{}_po'.format(case)]/df['{}_ce'.format(case)]-1)*100    
     return df
 
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+<!-- #region -->
+
 *Monopoly Power and Endogenous Product Variety: Distortions and Remedies* : production of welfare graphs.
 ========================================================================================================
+
 
 Installation
 ------------
@@ -8,19 +11,62 @@ Required programs to produce the graphs are
 - a recent distribution of scientific Python for instance [Anaconda Python](https://store.continuum.io/cshop/anaconda/)
 - the [dolo](https://github.com/EconForge/dolo) modeling package
 
+- Or run the code on Mybinder (link here) remote computer which has all the necesaary installations the the project folder `(No installation is needed)`
+
+Complete documentation with installation instructions for Dolo available at http://dolo.readthedocs.org/en/latest/
+
+
 Producing the graphs
 --------------------
 
-The code is contained in a [IPython](http://ipython.org/) notebook. To view and execute it:
-- Open a terminal and change the working directory so that it contains the `Produce Graphs.ipynb` file.
-- Launch `ipython notebook`. This should open a list of notebooks in a web browser. Click on `Produce Graphs.ipynb`.
-- Perform the computations using menu item `Cell/Run all` or execute each cell one by one with `shift+enter`.
+The code to produce the grapsh is contained in a [IPython](http://ipython.org/) notebook. The notebook will solve each version of the model with several values of the parameters. 
 
-The notebook will solve three versions of the baseline model (with CES-benassy, translog and exponential specifications) with
-several values of the parameters. The models are coded using `dolo` specifications in the `model_ces.yaml`, `model_translog.yaml` and `model_exponential` files.
+ To view and execute it on your __local computer__:
+- Open a terminal and change the working directory so that it contains the `Produce Graphs.ipynb` file.
+- Launch `ipython notebook`. This should open a list of notebooks in a web browser. Click on `ProduceGraphs.ipynb`.
+
+To view and execute it on the __remote computer__ just clicj on the`Produce Graphs.ipynb` notebook.
+
+##### The main notebook (Produce Graphs.ipynb)
+
+This notebook solve the various models for different parameter values and plots the associated welfare gains comparing competetitive equilibrium and planner equilibrium solutions. The models are coded using `dolo` specifications in the different `yaml` files in `model_files` folder. For the model solution code, see "BGM_solver" module. For more information on the perturbation method see [the documentation](https://dolo.readthedocs.io/en/latest/perturbations.html) or [the source code](https://github.com/EconForge/dolo/blob/master/dolo/algos/perturbation.py)
+
+On this notebook run each code block by "ctrl+enter" for the following computation steps
+
+
+1 Define various scenarios
+
+__Necesarry inputs__
+- modfile: name of the model file (ces, cesds, exponential etc.) See 'model_file' folders. Each file includes the model equations, parameters, calibrations, exogenous process. 
+
+- freeparam: name of the free parameter 
+
+- range: range of this parameter (starting value, end value, number of grid points)
+
+__Optional inputs__
+
+- fixedparam: name of the parameter (which will be different than the baseline calbiration) that you want to change and plot
+
+- value: value for this fixed parameter 
+
+2 Compute welfares calling the "BGM_solver" module. More information in the file itself, see comments at each step. This file
+- imports the model
+- sets the information such as the paramater to be changed at each step
+- solves the model for each case
+- return the results 
+
+3 Export results in a table (also export to excel)
+Number of columns is equal to N which is the number of different values of the free paramater. So each line will give the results for one specific parameter value.
+Each column computes
+
+Run each code block by "ctrl+enter"
+
+This is code 
+
 The welfare computations are based on a second order Taylor expansion of the decision-rule.
 
 Output
 ------
 
 The resulting graphs are stored in the `BGM_welfare_gains.*` files.
+<!-- #endregion -->
